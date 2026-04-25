db03618383e4b732be71f9a0b3faea95aafe522d
Yash2462 <patelyash2462@gmail.com>
2026-04-25 12:14:27 +0530

    refactor: general cleanup and routing restoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -17,97 +14,91 @@ import AcceptInvitation from "./pages/AcceptInvitation";
 import ForgotPassword from "./pages/ForgotPassword";
 import Analytics from "./pages/Analytics";
 import AnalyticsPage from "./pages/AnalyticsPage";
-import ErrorBoundary from "./components/ErrorBoundary";
-
 import { ThemeProvider } from "./components/theme-provider";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <ErrorBoundary>
+const App = () => {
+  return (
     <ThemeProvider defaultTheme="light" storageKey="projex-theme">
       <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route 
-                path="/dashboard" 
-                element={
-                  <>
-                    <Navigation />
-                    <Dashboard />
-                  </>
-                } 
-              />
-              <Route 
-                path="/projects" 
-                element={
-                  <>
-                    <Navigation />
-                    <Projects />
-                  </>
-                } 
-              />
-              <Route 
-                path="/subscription" 
-                element={
-                  <>
-                    <Navigation />
-                    <Subscription />
-                  </>
-                } 
-              />
-              <Route 
-                path="/analytics" 
-                element={
-                  <>
-                    <Navigation />
-                    <AnalyticsPage />
-                  </>
-                } 
-              />
-              <Route 
-                path="/settings" 
-                element={
-                  <>
-                    <Navigation />
-                    <Settings />
-                  </>
-                } 
-              />
-              <Route 
-                path="/projects/:id" 
-                element={
-                  <>
-                    <Navigation />
-                    <ProjectDetails />
-                  </>
-                } 
-              />
-              <Route 
-                path="/projects/:id/analytics" 
-                element={
-                  <>
-                    <Navigation />
-                    <Analytics />
-                  </>
-                } 
-              />
-              <Route path="/accept_invitation" element={<AcceptInvitation />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route 
+              path="/dashboard" 
+              element={
+                <>
+                  <Navigation />
+                  <Dashboard />
+                </>
+              } 
+            />
+            <Route 
+              path="/projects" 
+              element={
+                <>
+                  <Navigation />
+                  <Projects />
+                </>
+              } 
+            />
+            <Route 
+              path="/subscription" 
+              element={
+                <>
+                  <Navigation />
+                  <Subscription />
+                </>
+              } 
+            />
+            <Route 
+              path="/analytics" 
+              element={
+                <>
+                  <Navigation />
+                  <AnalyticsPage />
+                </>
+              } 
+            />
+            <Route 
+              path="/settings" 
+              element={
+                <>
+                  <Navigation />
+                  <Settings />
+                </>
+              } 
+            />
+            <Route 
+              path="/projects/:id" 
+              element={
+                <>
+                  <Navigation />
+                  <ProjectDetails />
+                </>
+              } 
+            />
+            <Route 
+              path="/projects/:id/analytics" 
+              element={
+                <>
+                  <Navigation />
+                  <Analytics />
+                </>
+              } 
+            />
+            <Route path="/accept_invitation" element={<AcceptInvitation />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
     </ThemeProvider>
-  </ErrorBoundary>
-);
+  );
+};
 
 export default App;
